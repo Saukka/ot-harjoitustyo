5a2b165bd5fa139d0833cc8dffa146f1c32a706a
Sauli Sorvari <sauli.sorvari@helsinki.fi>
2021-03-30 23:55:34 +0300

    .

---
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.unicafe;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author saulisorvari
+ */
+public class KassapaateTest {
+    
+    Kassapaate paate;
+    
+    public KassapaateTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        paate = new Kassapaate();
+    }
+    
+    @Test
+    public void maaraJaMyydytOikein() {
+       assertEquals(100000, paate.kassassaRahaa());
+       assertEquals(0, paate.maukkaitaLounaitaMyyty() + paate.edullisiaLounaitaMyyty());
+    }
+    @Test
+    public void kateisOstoMaukas() {
+        assertEquals(0, paate.syoMaukkaasti(400));
+        assertEquals(100400, paate.kassassaRahaa());
+        assertEquals(1,paate.maukkaitaLounaitaMyyty());
+    }
+    
+    @Test
+    public void kateisOstoEdullinen() {
+        assertEquals(0, paate.syoEdullisesti(240));
+        assertEquals(100240, paate.kassassaRahaa());
+        assertEquals(1,paate.edullisiaLounaitaMyyty());
+    }
+    
+    @Test
+    public void maksuEiRiittava() {
+        assertEquals(95, paate.syoEdullisesti(95));
+        assertEquals(100000, paate.kassassaRahaa());
+        assertEquals(0,paate.edullisiaLounaitaMyyty());
+    }
+    
+    
+    
+    
+}
--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,31 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void saldoAlussaOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    @Test
+    public void rahanLataaminenToimii() {
+        kortti.lataaRahaa(10);
+        assertEquals("saldo: 0.20", kortti.toString());
+    }
+ 
+    
+    // Jos tässä on @Test, ohjelman suorittaminen tuottaa errorin sillä alla oleva metodi ei ole void-metodi
+    public boolean rahanOttaminenToimii() {
+        kortti.otaRahaa(5);
+        if (kortti.saldo()<10) {
+            assertEquals("saldo: 0.05", kortti.toString());
+            return true;
+        } 
+        assertEquals("saldo: 0.10", kortti.toString());
+        return false;
+    }
+    
+    
+    
+    
+    
 }
