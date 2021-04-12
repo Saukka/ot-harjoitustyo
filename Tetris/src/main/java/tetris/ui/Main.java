d8d119d5176925bf44aa4e0cc5575b57e8b87683
Sauli Sorvari <sauli.sorvari@helsinki.fi>
2021-04-12 23:42:35 +0300

    Aloitusluokat

---
+++ Tetris/src/main/java/tetris/ui/Main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetris.ui;
+
+import tetris.ui.UserInterface;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInterface.main(args);
+    }
+}
---
+++ Tetris/src/main/java/tetris/ui/UserInterface.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetris.ui;
+
+import javafx.scene.layout.BorderPane;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.Scene;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+ 
+public class UserInterface extends Application {
+    
+    @Override
+    public void start(Stage window) {
+        
+        
+        window.setTitle("Tetris");
+        
+        Button button = new Button("A test button");
+                
+        BorderPane setUp = new BorderPane();
+        setUp.setCenter(button);
+        setUp.setTop(new Label("Toimiiko"));
+        
+        setUp.setPadding(new Insets(0,0,50,0));
+        
+        
+        Scene scene = new Scene(setUp,400,300);
+        
+        window.setScene(scene);
+        window.show();
+                
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
